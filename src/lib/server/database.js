7adf06ca223cd5c9ad715022ac6216a7f4b7aa20
hantanguyen <hantanguyen26@csu.fullerton.edu>
2024-10-26 21:12:23 -0700

    New Packages

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -2,8 +2,12 @@
 import postgres from 'postgres'
 
 // see env variables in .env
-import {PGCONNECT} from '$env/static/private';
+// import {PGCONNECT} from '$env/static/private';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const sql = postgres(PGCONNECT, {} )
+const sql = postgres(process.env.PGCONNECT, {});
+
+//const sql = postgres(PGCONNECT, {} )
 
 export default sql;
