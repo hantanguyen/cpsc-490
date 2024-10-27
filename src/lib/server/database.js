4189cff4db5db5260146aa0caa23f8210fef7ea7
hantanguyen <hantanguyen26@csu.fullerton.edu>
2024-10-26 23:07:19 -0700

    edits

--- src/lib/server/database.js
+++
@@ -1,8 +0,0 @@
-import postgres from 'postgres';
-import dotenv from 'dotenv';
-
-dotenv.config(); // Load the environment variables
-
-const sql = postgres(process.env.PGCONNECT, {}); // Use the environment variable
-
-export default sql;
