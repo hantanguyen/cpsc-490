73fba7509d46a75519176b175de048e87974deac
hantanguyen <hantanguyen26@csu.fullerton.edu>
2024-10-26 23:54:51 -0700

    comment

--- src/lib/server/database.js
+++
@@ -1,9 +0,0 @@
-
-//import postgres from 'postgres'
-
-// see env variables in .env
-// import {PGCONNECT} from '$env/static/private'; 
-
-// const sql = postgres(PGCONNECT, {})
-
-// export default sql;
